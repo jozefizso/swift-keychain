3c8c6cbbfa99b4ab106c9f43662fb4932549f940
Jozef Izso <izso@cisco.com>
2023-05-23 23:59:49 +0200

    Create package for Keychain library
    Targets Swift 3.0 and Xcode 8.2

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:3.0
+// Copyright 2023 Cisco Systems, Inc.
+// Licensed under MIT-style license (see LICENSE.txt file).
+
+import PackageDescription
+
+let package = Package(
+    name: "Keychain"
+    // platforms: [
+    //     .macOS(.v10_10),
+    //     .iOS(.v8),
+    //     .watchOS(.v2),
+    //     .tvOS(.v9)
+    // ]
+)
